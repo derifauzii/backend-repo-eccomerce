96eb27e0bd36c928659fe0bf455cba5da519acc7
derdorrr <derifauzi27@gmail.com>
2024-03-21 12:40:25 +0700

    commit ke 4

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     Public function Index() {
-        return view('user_template.home');
+        $allproducts = Product::latest()->get();
+
+        return view('user_template.home', compact('allproducts'));
     }
 }
